4ba7d0d2c04d39e23dcc284c9f3c7fa6fafa6a18
Pierre <pierre@yhapps.com>
2015-10-15 16:19:47 +0200

    Ajout du systeme de notif local

--- ionic-test/www/js/controllers.js
+++ ionic-test/www/js/controllers.js
@@ -1,20 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $cordovaLocalNotification, $ionicPlatform) {
 
     $scope.lastSyncArduino = "Never";
     $scope.lastSyncServer = "Never";
-    $scope.getDatetime = function() {
+    $scope.getDatetime = function () {
       return (new Date).getTime();
     };
 
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       $scope.lat = position.coords.latitude;
       $scope.lng = position.coords.longitude;
     });
 
-  })
+    $scope.scheduleSingleNotification = function () {
+      $cordovaLocalNotification.schedule({
+        id: 1,
+        title: 'Warning',
+        text: 'Youre so sexy!',
+        data: {
+          customProperty: 'custom value'
+        }
+      }).then(function (result) {
+        console.log('Notification 1 triggered');
+      })
+    }
 
+    })
 .controller('DatasCtrl', function($scope, Datas) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
