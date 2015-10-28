84e5f8180440d1b7b9184e062517e7649088481c
Pierre <pierre@yhapps.com>
2015-10-28 16:02:16 +0100

    Gestion d'erreurs bluetooth

--- ionic-test/www/js/controllers.js
+++ ionic-test/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $ionicPlatform, $cordovaLocalNotification, $cordovaGeolocation, $cordovaBluetoothSerial, $interval, $ionicLoading, $timeout) {
+  .controller('DashCtrl', function($scope, $ionicPlatform, $cordovaLocalNotification, $cordovaGeolocation, $cordovaBluetoothSerial, $interval, $ionicLoading, $timeout) {
 
     $scope.lastSyncArduino = "Never";
     $scope.lastSyncServer = "Never";
@@ -26,79 +26,85 @@ angular.module('starter.controllers', [])
           $scope.lat = lat;
           $scope.lng = long;
         }, function(err) {
-          // error
-          console.log("gps error", err);
+          $scope.lat = "Attente GPS ...";
+          $scope.lng = err;
         });
       setTimeout(pollCurrentLocation, 1000);
     };
 
-      $scope.devices = null;
-      $scope.listBluetoothDevices = function() {
-        $scope.bluetooth_label = "Scanning...";
-        // Setup the loader
-        $ionicLoading.show({
-          content: 'Scanning...',
-          animation: 'fade-in',
-          showBackdrop: true,
-          maxWidth: 200,
-          showDelay: 0
-        });
+    $scope.devices = null;
+    $scope.listBluetoothDevices = function() {
+      $scope.bluetooth_label = "Scanning...";
+
+      $ionicLoading.show({
+        content: 'Scanning...',
+        animation: 'fade-in',
+        showBackdrop: true,
+        maxWidth: 200,
+        showDelay: 0
+      });
+      bluetoothSerial.isEnabled(
         bluetoothSerial.discoverUnpaired(
-          function(unpaired_devices) {
-            $timeout(function(){
+          function (unpaired_devices) {
+            $timeout(function () {
               $scope.devices = unpaired_devices;
-              angular.forEach(unpaired_devices, function(device){
+              angular.forEach(unpaired_devices, function (device) {
                 console.log(device);
               });
               $scope.bluetooth_label = "Done.";
               $ionicLoading.hide();
             });
           },
-          function(){
+          function () {
             console.log("Bluetooth error.");
-          });
-      }
+          })
+      , function(){
+        $scope.bluetooth_label = "Bluetooth désactivé.";
+        $ionicLoading.hide();
+        console.log("Bluetooth error.");
+      });
+    }
 
     /*$interval(
-    $scope.scheduleSingleNotification = function () {
-      $cordovaLocalNotification.schedule({
-        id: 1,
-        title: 'Infos',
-        text: 'GPS: '+$scope.lat+ ' | ' + $scope.lng,
-        data: {
-          customProperty: 'custom value'
-        }
-      }).then(function (result) {
-        console.log('Notification error');
-      })
-    }, 10000);*/
+     $scope.scheduleSingleNotification = function () {
+     $cordovaLocalNotification.schedule({
+     id: 1,
+     title: 'Infos',
+     text: 'GPS: '+$scope.lat+ ' | ' + $scope.lng,
+     data: {
+     customProperty: 'custom value'
+     }
+     }).then(function (result) {
+     console.log('Notification error');
+     })
+     }, 10000);*/
 
     $ionicPlatform.ready(function() {
       pollCurrentLocation();
     });
 
-    })
-.controller('DatasCtrl', function($scope, Datas) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  })
+  .controller('DatasCtrl', function($scope, Datas) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.datas = Datas.all();
-  $scope.remove = function(data) {
-    Datas.remove(data);
-  };
-})
+    $scope.datas = Datas.all();
+    $scope.remove = function(data) {
+      Datas.remove(data);
+    };
+  })
 
-.controller('DataDetailCtrl', function($scope, $stateParams, Datas) {
-  $scope.data = Datas.get($stateParams.dataId);
-})
+  .controller('DataDetailCtrl', function($scope, $stateParams, Datas) {
+    $scope.data = Datas.get($stateParams.dataId);
+  })
 
-.controller('SettingsCtrl', function($scope) {
-  $scope.settings = {
-    enableDebugMode: true
-  };
-});
+  .controller('SettingsCtrl', function($scope) {
+    $scope.settings = {
+      enableDebugMode: true
+    };
+  });
